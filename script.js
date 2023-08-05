3214bbaca1f7a78059469255111d97d7f9d3e488
dan9banan <111860815+danilkass@users.noreply.github.com>
2023-08-05 19:01:00 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -15,81 +15,102 @@ function clear() {
 	finish = false;
 	result.textContent = 0;
 }
-
 document.getElementById('clear').onclick = clear;
+
+const video = document.querySelector('.video');
+	startVideo = function() {
+		video.classList.remove("hidden");
+		video.currentTime = 0.25;
+		video.play();
+		video.addEventListener("ended", () => {
+			video.classList.add("hidden");
+		 });
+		}
+
 
 document.querySelector('.calculator_keys').onclick = (event) => {
 	if(!event.target.classList.contains('key')) return;
 	if(event.target.classList.contains('c')) return;
-
+		
 	result.textContent = '';
-
+		
 	const btn = event.target.textContent
-
+		
 	if (digit.includes(btn)) {
-
+		
 		if (b==='' && sign === '') {
-		a += btn;
-		result.textContent = a;
+			a += btn;
+			result.textContent = a;
 		}
-
+		
 		else if (a!== '' && b!=='' && finish) {
-			b = btn;
-			finish = false;
-			result.textContent = b;
+				b = btn;
+				finish = false;
+				result.textContent = b;
 		}
-
+		
 		else{
 			b += btn;
 			result.textContent = b;
 		}
-
+		
 		console.log (a, b, sign);
 		return;
 	}
+		
 
 	if (action.includes(btn)) {
-		sign = btn;
-		result.textContent = sign;
-		console.log(sign);
-		return
-	}
 
-	const video = document.querySelector('.video');
-	if (btn === '=') {
-		if (b === '') b = a;
-		switch (sign) {
-			case '+':
-				a = (+a) + (+b);
-				break;
-			case '-':
-				a = a - b;
-				break;
-			case '*':
-				a = a * b;
-				break;
-			case '/':
-				if (b === '0'){
-					video.classList.remove("hidden");
-					video.currentTime = 0.25;
-					video.play();
-					video.addEventListener("ended", () => {
-						video.classList.add("hidden");
-					 });
+		
+		if (sign === '') {
+      // If not, set sign to btn and update the result.textContent
+     		sign = btn;
+      	result.textContent = sign;
+   		console.log(sign);
+    	} else {
+      // If yes, call the count() function
+      	count();
+			sign = btn;
+			result.textContent = sign
+			console.log(sign);
+   	} 
 
-
-					result.textContent = '0';
-					a = '';
-					b = '';
-					sign = '';
-					return;
-				}
-				a = a / b;
-				break;
-		}
-		finish = true;
-		result.textContent = a;
-		console.log (a, b, sign);
 	}
 	
+	if (btn === '=') count();
+
 }
+
+const count = function() {
+	switch (sign) {
+		case '+':
+			a = (+a) + (+b);
+			
+			break;
+		case '-':
+			a = a - b;
+			
+			break;
+		case '*':
+			a = a * b;
+			
+			break;
+		case '/':
+			if (b === '0'){
+				startVideo();
+				result.textContent = '0';
+				a = '';
+				
+				return;
+			}
+			a = (a / b).toFixed(1);
+			
+			break;
+	}
+	b = '';
+	sign = '';
+	finish = true;
+	result.textContent = a;
+	console.log (a, b, sign);
+}
+
